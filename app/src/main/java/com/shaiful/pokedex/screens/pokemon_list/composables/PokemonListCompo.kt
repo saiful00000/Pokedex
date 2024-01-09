e0c03ec6dd1675214c6b1eb83741d29a62ab97aa
shaiful <saiful161003@gmail.com>
2024-01-09 23:50:55 +0600

    Almoast show pokemon list

--- app/src/main/java/com/shaiful/pokedex/screens/pokemon_list/composables/PokemonListCompo.kt
+++ app/src/main/java/com/shaiful/pokedex/screens/pokemon_list/composables/PokemonListCompo.kt
@@ -1,12 +1,42 @@
 package com.shaiful.pokedex.screens.pokemon_list.composables
 
+import androidx.compose.animation.fadeIn
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.Coil
+import coil.ImageLoader
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.shaiful.pokedex.navigation.RouteNames
 import com.shaiful.pokedex.screens.pokemon_list.models.PokemonListEntry
 import com.shaiful.pokedex.screens.pokemon_list.viewmodels.PokemonListViewModel
 
@@ -16,14 +46,84 @@ fun PokemonListCompo() {
 }
 
 @Composable
-fun PokemonListItemCompo(
+fun PokemonListItemRowRow(
+    entryIndex: Int,
+    entries: List<PokemonListEntry>,
+    navController: NavController,
+) {
+    Column {
+        Row {
+            PokemonListEntryCompo(
+                entry = entries[entryIndex * 2],
+                navController = navController,
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            if(entries.size >= entryIndex * 2 + 2) {
+                PokemonListEntryCompo(
+                    entry = entries[entryIndex * 2 + 1],
+                    navController = navController,
+                    modifier = Modifier.weight(1f)
+                )
+            }else {
+                Spacer(modifier = Modifier.weight(1f))
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+}
+
+@Composable
+fun PokemonListEntryCompo(
     entry: PokemonListEntry,
     navController: NavController,
     modifier: Modifier = Modifier,
     pokemonListViewModel: PokemonListViewModel = hiltViewModel()
 ) {
     val defaultDominantColor = MaterialTheme.colorScheme.surface
-    var dominantColor = remember {
+    var dominantColor by remember {
         mutableStateOf(defaultDominantColor)
     }
+
+   Box (
+       contentAlignment = Alignment.Center,
+       modifier = Modifier
+           .shadow(5.dp, RoundedCornerShape(10.dp))
+           .clip(RoundedCornerShape(10.dp))
+           .aspectRatio(1f)
+           .background(
+               Brush.verticalGradient(
+                   colors = listOf(dominantColor, defaultDominantColor)
+               )
+           )
+           .clickable {
+               navController.navigate(
+                   "${RouteNames.pokemonDetailScreen}/${dominantColor.toArgb()}/${entry.pokemonName}"
+               )
+           }
+   ){
+       Column {
+           AsyncImage(
+               model = ImageRequest.Builder(LocalContext.current)
+                   .data(entry.imageUrl)
+                   .target {
+                       pokemonListViewModel.calcDominantColor(it) {color ->
+                           dominantColor = color
+                       }
+                   }
+                   .crossfade(true)
+                   .build(),
+               contentDescription = "entry_pokemon_image",
+               modifier = Modifier
+                   .size(120.dp)
+                   .align(Alignment.CenterHorizontally)
+           )
+           Text(
+               text = entry.pokemonName,
+               fontSize = 20.sp,
+               textAlign = TextAlign.Center,
+               modifier = Modifier.fillMaxWidth()
+           )
+       }
+   }
 }
--- app/src/main/java/com/shaiful/pokedex/screens/pokemon_list/viewmodels/PokemonListViewModel.kt
+++ app/src/main/java/com/shaiful/pokedex/screens/pokemon_list/viewmodels/PokemonListViewModel.kt
@@ -3,17 +3,79 @@ package com.shaiful.pokedex.screens.pokemon_list.viewmodels
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.capitalize
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.palette.graphics.Palette
+import com.shaiful.pokedex.data.remote.responses.pokemon_detail.Pokemon
 import com.shaiful.pokedex.repository.PokemonRepository
+import com.shaiful.pokedex.screens.pokemon_list.models.PokemonListEntry
+import com.shaiful.pokedex.util.ResData
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import java.util.Locale
 import javax.inject.Inject
 
 @HiltViewModel
 class PokemonListViewModel @Inject constructor(
     private val pokemonRepository: PokemonRepository
 ) : ViewModel() {
+
+    private val perPage = 10
+    private var currentPage = 0
+
+    var pokemonList = mutableStateOf<List<PokemonListEntry>>(emptyList())
+    var loadError = mutableStateOf("")
+    var isLoading = mutableStateOf(false)
+    var endReached = mutableStateOf(false)
+
+    fun loadPokemonPaginated() {
+        viewModelScope.launch {
+
+            isLoading.value = true
+
+            val result = pokemonRepository.getPokemonList(
+                limit = perPage,
+                offset = currentPage * perPage
+            )
+
+            when (result) {
+                is ResData.Error -> {
+                    endReached.value = currentPage * perPage >= result.data!!.count
+
+                    val pokemonEntries = result.data.results.mapIndexed { index, entry ->
+                        val number = if(entry.url.endsWith("/")) {
+                            entry.url.dropLast(1).takeLastWhile { it.isDigit() }
+                        } else {
+                            entry.url.takeLastWhile { it.isDigit() }
+                        }
+
+                        val url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${number}.png"
+                        PokemonListEntry(
+                            pokemonName = entry.name.capitalize(Locale.ROOT),
+                            imageUrl = url,
+                            number = number.toInt(),
+                        )
+
+                    }
+
+                    currentPage ++;
+                    loadError.value = ""
+                    isLoading.value = false
+
+                    pokemonList.value += pokemonEntries
+                }
+                is ResData.Success -> {
+                    loadError.value = result.message!!
+                    isLoading.value = false
+                }
+
+                is ResData.Loading -> TODO()
+            }
+        }
+    }
 
     fun calcDominantColor (drawable: Drawable, onFinish: (Color) -> Unit) {
         val bmp = (drawable as BitmapDrawable).bitmap.copy(Bitmap.Config.ARGB_8888, true)
